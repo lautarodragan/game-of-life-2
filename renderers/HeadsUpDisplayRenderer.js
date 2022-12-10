95db734001b646ce715150a6f3a3b123ceaf00d9
taro <lautaro@airtm.io>
2022-12-10 19:16:05 -0300

    fix: text alignment

--- renderers/HeadsUpDisplayRenderer.js
+++ renderers/HeadsUpDisplayRenderer.js
@@ -8,28 +8,37 @@ export const HeadsUpDisplayRenderer = (gl) => {
   const program = HeadsUpDisplayProgram(gl)
   const charSize = 8
   const textZoom = 4
+  const resolution = { width: 0, height: 0}
   let fps = 0
 
   function render() {
     if (!program.areTexturesLoaded())
       return
     
+    program.use()
     const text = ('FPS ' + Math.round(fps).toString()).toUpperCase()
+  
+    renderTextAligned(text, 'bottom-left')
+    renderTextAligned(text, 'top-left')
+    renderTextAligned(text, 'bottom-right')
+    renderTextAligned(text, 'top-right')
+  }
+  
+  function renderText(text, x, y) {
     const positions = new Float32Array(text.length * componentsPerCharacter)
     const textureCoords = new Float32Array(text.length * componentsPerCharacter)
-    
+  
     for (let i = 0; i < text.length; i++) {
       const letterIndex = text.codePointAt(i) - 65 + 33 // A=65. the texture has 33 special chars before A.
-      const position = sizedRecToScreenCoords(charSize * i * textZoom, 0, charSize * textZoom, charSize * textZoom)
+      const position = sizedRecToScreenCoords(x + charSize * i * textZoom, y, charSize * textZoom, charSize * textZoom)
       const texCoords = textureCoordsSprite(letterIndex, 0, 83, 1)
-      
+    
       for (let j = 0; j < componentsPerCharacter; j++) {
         positions[i * componentsPerCharacter + j] = position[j]
         textureCoords[i * componentsPerCharacter + j] = texCoords[j]
       }
     }
   
-    program.use()
     program.setPositions(positions)
     program.setTextureCoords(textureCoords)
     program.render(positions.length / 2)
@@ -39,9 +48,32 @@ export const HeadsUpDisplayRenderer = (gl) => {
     fps = _
   }
   
+  function setResolution(width, height) {
+    resolution.width = width
+    resolution.height = height
+    program.setResolution(width, height)
+  }
+  
+  function alignText(text, alignment = 'bottom-left') {
+    if (alignment === 'bottom-left')
+      return [0, 0]
+    if (alignment === 'top-left')
+      return [0, resolution.height - charSize * textZoom]
+    if (alignment === 'bottom-right')
+      return [resolution.width - text.length * charSize * textZoom, 0]
+    if (alignment === 'top-right')
+      return [resolution.width - text.length * charSize * textZoom, resolution.height - charSize * textZoom]
+    throw new Error(`Invalid alignment "${alignment}".`)
+  }
+  
+  function renderTextAligned(text, alignment) {
+    const textAlignment = alignText(text, alignment)
+    renderText(text, textAlignment[0], textAlignment[1])
+  }
+  
   return {
     render,
-    setResolution: program.setResolution,
+    setResolution,
     setFPS,
     loadFontTexture: program.loadFontTexture,
   }
