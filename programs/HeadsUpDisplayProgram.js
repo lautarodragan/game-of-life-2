377bea4ec621f05b9a39d997dfa8dbfc1836c62d
taro <lautaro@airtm.io>
2022-12-12 22:15:10 -0300

    fix: text texture bleeding

--- programs/HeadsUpDisplayProgram.js
+++ programs/HeadsUpDisplayProgram.js
@@ -103,6 +103,8 @@ export const HeadsUpDisplayProgram = (gl) => {
     gl.bindTexture(gl.TEXTURE_2D_ARRAY, fontTexture)
     gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texImage3D(
       gl.TEXTURE_2D_ARRAY,
       0,
